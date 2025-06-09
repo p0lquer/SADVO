2e2086edd3709ea1d117a93c74423099e446958b
p0lquer <cristianbeltre10@gmail.com>
2025-06-09 19:37:41 -0400

    cam

--- Persistence/Data/Configurations/AlianzaPoliticaConfig.cs
+++ Persistence/Data/Configurations/AlianzaPoliticaConfig.cs
@@ -38,7 +38,7 @@ namespace SADVO.Persistence.Data.Configurations
                    .HasForeignKey(a => a.PartidoReceptorId)
                    .OnDelete(DeleteBehavior.Restrict);
             
-            // Restricción única para evitar solicitudes duplicadas
+            // Restricción única para evitar duplicados
                         builder.HasIndex(a => new { a.PartidoSolicitanteId, a.PartidoReceptorId })
                 .IsUnique();
         }
