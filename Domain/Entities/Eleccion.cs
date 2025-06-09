e10d4285ecf8d27f6b6640eec2f3ebb629609952
p0lquer <cristianbeltre10@gmail.com>
2025-06-09 19:40:20 -0400

    //

--- Domain/Entities/Eleccion.cs
+++ Domain/Entities/Eleccion.cs
@@ -6,11 +6,12 @@ namespace SADVO.Domain.Entities
 {
    public class Eleccion : CommonEntity<int>
     {
+        
 
         public required DateTime FechaOcurrida { get; set; }
 
         public  required Partido_Politico PartidoPolitico { get; set; }  
 
         public required ICollection<Puesto_Electivo> PuestoElectivo { get; set; }
-    }
+   }
 }
